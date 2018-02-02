2e55408a6a681b25edd5f0a4e9740f9ae3356da9
Fahad Almusharraf <ifahad7@gmail.com>
2018-02-01 19:26:32 -0500

    Fix maven build and improve otp node

--- src/main/kotlin/edu/uoft/ece/tattan/OtpNode.kt
+++ src/main/kotlin/edu/uoft/ece/tattan/OtpNode.kt
@@ -1,31 +1,81 @@
 package edu.uoft.ece.tattan
 
-import edu.stanford.nlp.simple.*;
-import com.ericsson.otp.erlang.*;
+import edu.stanford.nlp.simple.*
+import com.ericsson.otp.erlang.*
+
+fun preloadNLPService() {
+    val sentence = Sentence("something something")
+    sentence.sentiment()
+}
+
+fun sentiment(utterance: OtpErlangObject): OtpErlangTuple {
+    if (utterance is OtpErlangBinary) {
+
+        val utteranceString = utterance.toString()
+        val sentence = Sentence(utteranceString)
+
+        return when (sentence.sentiment()) {
+            SentimentClass.VERY_NEGATIVE ->
+                OtpErlangTuple(arrayOf(OtpErlangAtom("ok"), OtpErlangAtom("very_negative")))
+            SentimentClass.NEGATIVE ->
+                OtpErlangTuple(arrayOf(OtpErlangAtom("ok"), OtpErlangAtom("negative")))
+            SentimentClass.NEUTRAL ->
+                OtpErlangTuple(arrayOf(OtpErlangAtom("ok"), OtpErlangAtom("neutral")))
+            SentimentClass.POSITIVE ->
+                OtpErlangTuple(arrayOf(OtpErlangAtom("ok"), OtpErlangAtom("positive")))
+            SentimentClass.VERY_POSITIVE ->
+                OtpErlangTuple(arrayOf(OtpErlangAtom("ok"), OtpErlangAtom("very_positive")))
+            else ->
+                errorTuple("Not valid utterance")
+        }
+    } else {
+        return errorTuple("Not valid utterance")
+    }
+}
+
+fun errorTuple(message: String): OtpErlangTuple {
+    return OtpErlangTuple(arrayOf(OtpErlangAtom("error"), OtpErlangString(message)))
+}
+
+fun receiveMessages(node: OtpNode) {
+    val process = node.createMbox("NLP_MBOX")
+
+    println("Process ${process.self()} is running on $node")
+
+    while (true) {
+        val incomingTuple = process.receive() as OtpErlangTuple
+        val senderPid = incomingTuple.elementAt(0) as OtpErlangPid
+
+        println("Received from: $senderPid --> $incomingTuple")
+
+        val requestName = incomingTuple.elementAt(1)
+
+        if (requestName is OtpErlangAtom && incomingTuple.arity() == 3) {
+            val result: OtpErlangObject = when (requestName.atomValue()) {
+                "sentiment" -> sentiment(incomingTuple.elementAt(2))
+                else -> errorTuple("unknown request name")
+            }
+            process.send(senderPid, result)
+        } else {
+            process.send(senderPid, errorTuple("wrong message format, should be {pid, :request, data}"))
+        }
+
+    }
+}
 
 fun main(args: Array<String>) {
     var node: OtpNode? = null
 
-    try {
-        node = OtpNode("NLP_NODE")
-        node.setCookie("SUPER_SECRET")
-
-        val mailBox = node.createMbox("NLP_MBOX")
-
-        val incomingTuple : OtpErlangTuple
-        while (true) {
-            val incomingTuple = mailBox.receive() as OtpErlangTuple
-            val requestName = incomingTuple.elementAt(0) as OtpErlangAtom
-            val requestData = incomingTuple.elementAt(1) as OtpErlangMap
-
-            print(requestName.toString())
-            print(requestData.toString())
+    while (true) {
+        try {
+            node = OtpNode("NLP_NODE")
+            preloadNLPService()
+            receiveMessages(node)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            node?.close()
         }
-    } catch (e: Exception) {
-        e.printStackTrace()
-    } finally {
-       node?.close()
-
     }
 }
 
